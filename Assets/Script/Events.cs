8e326b71d51a5c443ac379753fd947b2a06800ab
Juju <julienrippstein@hotmail.com>
2020-06-29 18:17:21 +0200

    Basket + spawner + buttons
    Todo :
    Score to add

--- Assets/Script/Events.cs
+++ Assets/Script/Events.cs
@@ -3,13 +3,36 @@ using Valve.VR.InteractionSystem;
 
 public class Events : MonoBehaviour
 {
+    public GameObject ballPrefab;
+    public Transform pointSpawn;
+
     public void OnPress(Hand hand)
     {
         Debug.Log("SteamVR Button pressed!");
+
+        if (ballPrefab == null)
+        {
+            return;
+        }
+        else
+        {
+            GameObject balloon = Instantiate(ballPrefab, pointSpawn.position, pointSpawn.rotation) as GameObject;
+        }
+        
     }
 
+    //No VR button
     public void OnCustomButtonPress()
     {
         Debug.Log("We pushed our custom button!");
+
+        if (ballPrefab == null)
+        {
+            return;
+        }
+        else
+        {
+            GameObject balloon = Instantiate(ballPrefab, pointSpawn.position, pointSpawn.rotation) as GameObject;
+        }
     }
 }
