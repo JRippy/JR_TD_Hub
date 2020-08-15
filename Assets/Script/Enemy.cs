65f6a2fb3b6ca2e305b945aaaf637acc95668557
Julien R <julienrippstein@hotmail.com>
2020-08-15 19:47:51 +0200

    Todo Camera
    Camera security and score in progress

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -16,11 +16,13 @@ public class Enemy : MonoBehaviour
     private bool hurt = false;
     private bool dead = false;
     private bool invul = false;
+    private Rigidbody rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         target = Waypoints.points[0];
+        rigidbody = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -34,7 +36,28 @@ public class Enemy : MonoBehaviour
         {
             Vector3 dir = target.position - transform.position;
             transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
-            animator.SetFloat("Speed", dir.normalized.x + dir.normalized.z);
+            animator.SetFloat("Speed", Mathf.Abs(dir.normalized.x + dir.normalized.z));
+
+
+
+
+            //// Determine which direction to rotate towards
+            //Vector3 targetDirection = target.position - transform.position;
+
+            //// The step size is equal to speed times frame time.
+            //float singleStep = speed * Time.deltaTime;
+
+            //// Rotate the forward vector towards the target direction by one step
+            //Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+            //// Draw a ray pointing at our target in
+            //Debug.DrawRay(transform.position, newDirection, Color.red);
+
+            //// Calculate a rotation a step closer to the target and applies rotation to this object
+            //transform.rotation = Quaternion.LookRotation(newDirection);
+
+            //Vector3 targetDirection = new Vector3(target.position.x, this.transform.position.y, target.position.z);
+            //transform.LookAt(targetDirection);
         }
         else
         {
@@ -45,6 +68,8 @@ public class Enemy : MonoBehaviour
         if (Vector3.Distance(transform.position, target.position) <= 0.3f)
         {
             GetNextWaypoint();
+            Vector3 targetDirection = new Vector3(target.position.x, this.transform.position.y, target.position.z);
+            transform.LookAt(targetDirection);
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -70,11 +95,38 @@ public class Enemy : MonoBehaviour
             print("Laser");
             zombieElectrocut();
         }
+        else if (other.tag == "Shield")
+        {
+            //rigidbody.freezeRotation = true;
+            zombieAttack();
+            moving = false;
+
+        }
+        else if (other.tag == "ShieldDestroyed")
+        {
+            //rigidbody.constraints = RigidbodyConstraints.
+            //rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+            zombieEndAttack();
+
+        }
         //else if (other.tag == "Base")
         //{
         //    print("Base");
         //    zombieAttack();
         //}
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+        Debug.Log(other.tag);
+        Debug.Log(other.gameObject.name);
+        if (other.tag == "Shield")
+        {
+            //rigidbody.freezeRotation = true;
+            //other.gameObject.SetActive(false);
+
+        }
     }
 
     private void GetNextWaypoint()
@@ -97,7 +149,15 @@ public class Enemy : MonoBehaviour
         attack = true;
         moving = false;
 
-        animator.SetBool("Attack",true);
+        animator.SetBool("Attack", true);
+    }
+
+    private void zombieEndAttack()
+    {
+        attack = false;
+        moving = true;
+
+        animator.SetBool("Attack", false);
     }
 
     private void zombieHurt()
